fb69edaf37af3fc290b944eddf223275432228cb
Rolfrider <ravkwiatkowski@gmail.com>
2018-03-06 19:19:17 +0100

    Initial commit First task

---
+++ src/BinaryGap.java
@@ -0,0 +1,18 @@
+// Task : https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+
+public class BinaryGap {
+    int solution(int N){
+        String num = Integer.toString(N, 2);
+        int maxSlice = 0;
+        int currentSlice = 0;
+        for (int i = 0 ; i < num.length(); i++){
+            if(num.charAt(i) == '0')
+                currentSlice++;
+            else {
+                maxSlice = Math.max(currentSlice, maxSlice);
+                currentSlice = 0;
+            }
+        }
+        return maxSlice;
+    }
+}
