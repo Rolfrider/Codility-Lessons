f52f8b457d18b733f847560d209e1c6b82204f81
Rolfrider <ravkwiatkowski@gmail.com>
2018-03-12 21:03:53 +0100

    not finished

---
+++ src/CountingElements/FrogRiverOne.java
@@ -0,0 +1,18 @@
+package CountingElements;
+
+public class FrogRiverOne {
+    public int solution(int X, int [] A){
+        int seconds = -1;
+        boolean [] positions = new boolean[X + 1];
+        positions[0] = true;
+
+        for (int i = 0; i < A.length; i++) {
+
+        }
+
+        return seconds;
+    }
+
+
+
+}
